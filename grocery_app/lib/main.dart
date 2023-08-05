2f3b43919caa110cf42f7103f2c89f7f79cc31b8
unknown <phitruong170901@gmail.com>
2023-08-05 18:19:57 +0700

    LyThuyetBuoi7

---
+++ grocery_app/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:grocery_app/constants.dart';
+import 'package:grocery_app/screens/home/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.all(defaultPadding * 0.75),
+            shape: const StadiumBorder(),
+            backgroundColor: primaryColor,
+          ),
+        ),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ testla_car_control_app/android/app/src/main/kotlin/com/example/testla_car_control_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testla_car_control_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
